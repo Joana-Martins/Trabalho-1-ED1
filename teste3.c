78df9a9cf567a3b051027332fddc981d33c3eadc
Joana-Santos-Martins <44272378+Joana-Santos-Martins@users.noreply.github.com>
2019-05-08 16:47:53 -0300

    Add files via upload

--- teste3.c
+++ teste3.c
@@ -29,6 +29,8 @@ int seta(){
 }
 void dificuldade (int i){
   switch (i){
+    case 7:
+    break;
     case 0:
       system("clear");
       printf("================================== BISCA ==================================\n\n\n");
@@ -97,52 +99,111 @@ void dificuldade (int i){
 void selecionado(int i){
   switch (i) {
     case 0:
-      printf(" \nSelecionado-> DIFICULDADE: - JOGADORES: - \n");
-      printf(" - Se nao escolhido sera o default -");
+      printf("\n \nSelecionado-> DIFICULDADE: Facil JOGADORES: 2 \n");
       break;
     case 1:
-      printf(" \nSelecionado-> DIFICULDADE: Facil JOGADORES: - \n");
+      printf("\n\nSelecionado-> DIFICULDADE: Facil JOGADORES: 4 \n");
       break;
     case 2:
-      printf(" \nSelecionado-> DIFICULDADE: Facil JOGADORES: 2 \n");
-      printf(" - Se nao escolhido sera o default -");
+      printf("\n\nSelecionado-> DIFICULDADE: Dificil JOGADORES: 2 \n");
       break;
-    case 3
-      printf(" \nSelecionado-> DIFICULDADE: Facil JOGADORES: 4 \n");
-      printf(" - Se nao escolhido sera o default -");
-    case 4:
-      printf(" \nSelecionado-> DIFICULDADE: Dificil JOGADORES: - \n");
-      printf(" - Se nao escolhido sera o default -");
-    case 5:
-      printf(" \nSelecionado-> DIFICULDADE: Dificil JOGADORES: 2 \n");
-      printf(" - Se nao escolhido sera o default -");
-    case 6:
-      printf(" \nSelecionado-> DIFICULDADE: Dificil JOGADORES: - \n");
-      printf(" - Se nao escolhido sera o default -");
-    case 7:
-      printf(" \nSelecionado-> DIFICULDADE: - JOGADORES: 2 \n");
-      printf(" - Se nao escolhido sera o default -");
-    case 8:
-      printf(" \nSelecionado-> DIFICULDADE: - JOGADORES: 4 \n");
-      printf(" - Se nao escolhido sera o default -");
+    case 3:
+      printf(" \n\nSelecionado-> DIFICULDADE: Dificil JOGADORES: 4 \n");
+      break;
     default:
     break;
   }
 }
 
 int main(){
-  int d, j;
-  dificuldade(0);
-  slecionado(0);
+  int d = 0, j = 0, i = 0;
+
+  int x = 1, y = 1;
+  dificuldade(i);
+  selecionado(i);
   while(1){
     switch(getch()){
       case '\033':
-        if(seta() == 0){
+        if(seta() == 1){
+          if( i == 0){
+            i = 1;
+            x = 1;
+          dificuldade(i);
+          selecionado(d);
 
+        }else if(i == 1){
+          i = 2;
+          x = 2;
+          dificuldade(i);
+          selecionado(d);
+        }else if(i == 2){
+            i = 3;
+            y = 1;
+            dificuldade(i);
+            selecionado(d);
+          }else if(i == 3){
+            i = 4;
+            y = 2;
+            dificuldade(i);
+            selecionado(d);
+          }
+        }else{
+          if( i == 1){
+            i = 0;
+            dificuldade(i);
+            selecionado(d);
+
+          }else if(i == 2){
+            i = 1;
+            x = 1;
+            dificuldade(i);
+            selecionado(d);
+
+          }else if(i == 3){
+            i = 2;
+            x = 2;
+            dificuldade(i);
+            selecionado(d);
+
+          }else if(i == 4){
+            i = 3;
+            y = 1;
+            dificuldade(i);
+            selecionado(d);
+            }
         }
+        break;
       case '\n':
+        if(x == 1 && y == 1){
+          d = 0;
+          system("clear");
+          dificuldade(0);
+          selecionado(d);
+        }else if ( x == 1 && y == 2){
+          d = 1;
+          system("clear");
+          dificuldade(0);
+          selecionado(d);
+        }else if( x == 2 && y == 1){
+          d = 2;
+          system("clear");
+          dificuldade(0);
+          selecionado(d);
+        }else if(x == 2 && y == 2){
+          d = 3;
+          system("clear");
+          dificuldade(0);
+          selecionado(d);
+        }
+        break;
       case '1':
+      system("clear");
+      printf("\nIniciando jogo\n");
+      break;
       case '0':
+      printf("\n");
+      exit(0);
+      break;
       default:
       break;
     }
